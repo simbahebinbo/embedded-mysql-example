b884f470d484547733bfcfcfac5eabc8c7d4d267
simbahebinbo <lansheng228@protonmail.com>
2021-11-15 18:21:49 +0800

    升级

--- src/test/java/com/singlemusic/example/DatabaseTest.java
+++ src/test/java/com/singlemusic/example/DatabaseTest.java
@@ -1,21 +1,29 @@
 package com.singlemusic.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.math.BigInteger;
 
+
+@Slf4j
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = EmbeddedMysqlConfiguration.class)
+@SpringBootTest(classes = EmbeddedMysqlConfiguration.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@EnableConfigurationProperties
 public class DatabaseTest {
 
-    @Resource
+    @Autowired
     private EntityManager entityManager;
 
     @Test
